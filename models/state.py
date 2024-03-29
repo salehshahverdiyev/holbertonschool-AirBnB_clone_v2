7fb8993d224f519f377a896009675a93bbfb3e2b
Tunzale1 <tunzaleimanova03@mail.ru>
2024-03-29 20:45:53 +0000

    dbstorage states and cities updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from models.engine.file_storage import FileStorage
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def cities(self):
+        """Getter function for cities"""
+        storage = FileStorage()
+        return [city for city in storage.all(City).values()
+                if city.state_id == self.id]
