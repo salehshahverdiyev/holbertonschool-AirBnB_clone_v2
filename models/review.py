90f5d0c4306ffe5534fac3bcf57ed19453049fc7
Tunzale1 <tunzaleimanova03@mail.ru>
2024-04-01 16:47:40 +0000

    review stored!

--- models/review.py
+++ models/review.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
+
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
