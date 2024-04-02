d624a1304bc3bc14882195f486f14d86cf55ce11
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-04-02 10:20:27 +0400

    try to fix import error

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Test Amenity class."""
+from tests.test_models.test_base_model import BaseModel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenity(BaseModel):
+    """Test Amenity class."""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialize instance."""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Test name attribute."""
         new = self.value()
         self.assertEqual(type(new.name), str)
