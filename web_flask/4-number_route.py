8bb6ff5a94d55697b6917e822605852409fee5a7
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-04-14 09:28:57 +0400

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Script Documentation
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """
+    Method Documentation
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Method Documentation
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    Method Documentation
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """
+    Method Documentation
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    Method Documentation
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
